b19ab3b333f3c586da69bea3e064c2d93a8be9ee
Greg Weber <gregoryweberguitars@gmail.com>
2018-09-15 17:34:27 -0700

    lezrned a few was to conditionally render a component

--- counter-app-tutorial/src/components/counter.jsx
+++ counter-app-tutorial/src/components/counter.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import { list } from 'postcss';
  
 class Counter extends Component {
     state = { 
         count: 0,
+        tags: ['tag1', 'tag2', 'tag3'],
     };
+
+    renderTags() {
+        if (this.state.tags.length === 0) {
+            return <p>There are no tags</p>
+        }
+        return this.state.tags.map(tag => <li key={tag}>{tag}</li>)
+    }
 
     render() { 
         return (
             <React.Fragment>
                 <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
                 <button className="btn btn-secondary btn-small">Increment</button>
+                <ul>
+                    { this.renderTags() }
+                </ul>
             </React.Fragment>
         );
     }
