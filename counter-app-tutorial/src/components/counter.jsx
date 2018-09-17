1bc4d369626ec7ac4a458c8bcd79fac38b13c42d
Greg Weber <gregoryweberguitars@gmail.com>
2018-09-17 16:10:06 -0700

    learned about c0omposing components out of other componenets and passing props

--- counter-app-tutorial/src/components/counter.jsx
+++ counter-app-tutorial/src/components/counter.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { list } from 'postcss';
+// import { list } from 'postcss';
 
 
 class Counter extends Component {
     state = {  
-        count: 0,
+        value: this.props.value
         // tags: ['tag1', 'tag2', 'tag3'],
     };
 
@@ -16,30 +16,31 @@ class Counter extends Component {
     // }
     handleIncrement = product => {
         console.log(product);
-        this.setState({count: this.state.count + 1})
+        this.setState({value: this.state.value + 1})
     }
 
     render() { 
         return (
-            <React.Fragment>
+            <div>
+                {/* {this.props.children} */}
                 <span className={this.getBadgeClasses()}> {this.formatCount()}</span>
-                <button onClick={ () => this.handleIncrement(product) } className="btn btn-secondary btn-small">Increment</button>
+                <button onClick={ () => this.handleIncrement() } className="btn btn-secondary btn-small">Increment</button>
                 {/* <ul>
                     { this.renderTags() }
                 </ul> */}
-            </React.Fragment>
+            </div>
         );
     }
 
     getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
+        const { value } = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
 
